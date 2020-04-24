abfa4d7114618ab9f67c3c1409bdc356d0c093cb
jarodriguez045 <5765093-jarodriguez045@users.noreply.gitlab.com>
2020-04-24 17:18:22 -0400

    commit

--- Flask/app.py
+++ Flask/app.py
@@ -1,12 +1,11 @@
 # /bit_flask/flask/app.py
 
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
