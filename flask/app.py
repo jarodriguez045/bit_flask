9d0aaf791d75e0c7e3a7130464a678d3ac92b915
Jose_Ubuntu <jarodriguez045@gmail.com>
2020-04-26 15:29:31 -0400

    commit website

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,12 @@
-# /bit_flask/flask/app.py
-
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Luke!"
+@app.route('/')
+def hello_world():
+    author = "Jose"
+    name = "Rodriguez"
+    return render_template('index.html', author=author, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    # app.run configured for running out of docker
